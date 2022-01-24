18c8cc4a283721281761df4d320ad0e68e53d848
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-01-24 23:31:32 +0100

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id int NOT NULL PRIMARY KEY, 
+    name char(50),
+    date_of_birth date,
+    escape_attempts int,
+    neutered BOOLEAN NOT NULL, 
+    weight_kg int NOT NULL
 );
